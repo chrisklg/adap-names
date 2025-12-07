2fd2db209a72e5ee9fb5a79e0529cf795f6737f9
Christoph Klingenberg <christoph-klingenberg@web.de>
2025-12-07 16:49:31 +0100

    override findNodes

--- src/adap-b05/files/Directory.ts
+++ src/adap-b05/files/Directory.ts
@@ -1,7 +1,7 @@
 import { Node } from "./Node";
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
 
 export class Directory extends Node {
-
     protected childNodes: Set<Node> = new Set<Node>();
 
     constructor(bn: string, pn: Directory) {
@@ -20,4 +20,32 @@ export class Directory extends Node {
         this.childNodes.delete(cn); // Yikes! Should have been called remove
     }
 
+    /**
+     * Override findNodes to recursively search this directory and all children
+     * @param bn basename of node being searched for
+     */
+    public override findNodes(bn: string): Set<Node> {
+        IllegalArgumentException.assert(
+            bn != null && bn != undefined,
+            "basename must not be null or undefined"
+        );
+
+        // Check if this directory itself matches
+        const result: Set<Node> = super.findNodes(bn);
+
+        // recursively search all child nodes
+        for (const child of this.childNodes) {
+            try {
+                const childMatches = child.findNodes(bn);
+                // Add all matches from child to result set
+                for (const match of childMatches) {
+                    result.add(match);
+                }
+            } catch (ex) {
+                throw ex;
+            }
+        }
+
+        return result;
+    }
 }
